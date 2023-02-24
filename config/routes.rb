52144c3ceaedeb8154356f77c63196b4c533cb9b
eliot rafat offor <eliot.rafat.offor@gmail.com>
2023-02-24 15:37:05 +0100

    pasouf

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/lists', to: 'lists#index', as: :lists
-  get '/lists/:id', to: 'lists#show', as: :list
-
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:delete]
 end
